55b943f55f8b3bec4c752a5c57db239c94cf8458
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2025-08-07 12:30:20 +0200

    Added keys to reminders, so we can filter them by a 6 long key.

--- cogs/reminders.py
+++ cogs/reminders.py
@@ -1,9 +1,14 @@
 import discord
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta
+from random import choice
+from string import ascii_lowercase, ascii_uppercase
 
 from util import is_discord_member
 from timeutil import UserFriendlyTime
+
+
+KEY_LEN: int = 6
 
 class Reminders(commands.Cog):
     def __init__(self, bot):
@@ -20,6 +25,7 @@ class Reminders(commands.Cog):
             channel_id=ctx.channel.id,
             message=message,
             timestamp=time.dt
+            key=''.join(choice(ascii_lowercase + ascii_uppercase, KEY_LEN))
         )
         await ctx.reply(f"{ctx.author.mention}: I will remind you at {time.dt.strftime('%Y-%m-%d %H:%M:%S')} UTC with the message: {message}")
 
@@ -32,9 +38,9 @@ class Reminders(commands.Cog):
             return await ctx.reply(f"{ctx.author.display_name}: You have no reminders set.")
         elif len(reminders) < 25:
             embed = discord.Embed(title=f"{ctx.author.display_name}'s Reminders", color=discord.Color.blue())
-            for message, _, timestamp in reminders:
+            for message, _, timestamp, key in reminders:
                 embed.add_field(
-                    name=f"Reminder at {timestamp.strftime('%Y-%m-%d %H:%M:%S')}",
+                    name=f"Reminder at {timestamp.strftime('%Y-%m-%d %H:%M:%S')}; key={key}",
                     value=f"Message: {message}",
                     inline=False
                 )
@@ -55,14 +61,21 @@ class Reminders(commands.Cog):
 
     @is_discord_member()
     @commands.command(name='delete_reminder', aliased=['delreminder'])
-    async def delete_reminder(self, ctx, *, timestamp: UserFriendlyTime) -> None:
-        """Delete all reminders of the user with the given timestamp."""
+    async def delete_reminder(self, ctx, *, key: str) -> None:
+        """Delete all reminders of the user with the given key."""
+        if len(key) != KEY_LEN:
+            return await ctx.reply(f"{ctx.author.display_name}: Key does not meet criteria, expected {KEY_LEN} long.")
+
+        for char in key:
+            if not char in ascii_lowercase + ascii_uppercase:
+                return await ctx.reply(f"{ctx.author.display_name}: Key contains non alphabetical characters.")
+        
         reminders = await self.bot.database.get_reminders(ctx.author.id)
         if not reminders:
             return await ctx.reply(f"{ctx.author.display_name}: You have no reminders to delete.")
-
-        await self.bot.database.pop_reminder(ctx.author.id, timestamp)    
-        await ctx.reply(f"{ctx.author.mention}: Reminder(s) deleted successfully.") # We could reply something like "@...: 4 Reminders deleted successfully."
+        
+        await self.bot.database.pop_reminder(user_id=ctx.author.id, key=key)    
+        await ctx.reply(f"{ctx.author.mention}: Reminder( deleted successfully.")
 
 async def setup(bot):
     await bot.add_cog(Reminders(bot))
